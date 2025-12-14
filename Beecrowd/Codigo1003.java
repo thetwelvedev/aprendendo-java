a634fce8b091f0e1f2cfe1eb4080a9ad24be9474
Leonardo Castro <twelve.programming@gmail.com>
2025-12-14 16:30:00 -0400

    Código 1003

---
+++ Beecrowd/Codigo1003.java
@@ -0,0 +1,18 @@
+package Beecrowd;
+
+import java.util.Scanner;
+
+public class Codigo1003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int SOMA = A + B;
+
+        System.out.println("SOMA = " + SOMA);
+
+        sc.close();
+    }
+}
