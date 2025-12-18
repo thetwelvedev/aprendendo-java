3218f6380162f6bbb54c88470cb2d9c821da94c8
Leonardo Castro <twelve.programming@gmail.com>
2025-12-18 16:30:00 -0400

    Código 1005

---
+++ Beecrowd/Codigo1005.java
@@ -0,0 +1,18 @@
+package Beecrowd;
+
+import java.util.Scanner;
+
+public class Codigo1005 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        double media = (A * 3.5 + B * 7.5) / 11.0;
+
+        System.out.printf("MEDIA = %.5f%n", media);
+
+        sc.close();
+    }
+}
